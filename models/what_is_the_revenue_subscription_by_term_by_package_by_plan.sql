b67a1e07d1cdb93ee84ab0aff01d19d602927550
brandon-pm61 <brandon-pm61>
2022-05-26 17:48:36 +0000

    kl_commit_dbt

---
+++ models/what_is_the_revenue_subscription_by_term_by_package_by_plan.sql
@@ -0,0 +1,48 @@
+{{config(
+	catalog = "hive",
+	schema = "promethium",
+	materialized = "view"
+)}}
+WITH select_step1 as (
+  SELECT
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"."PLAN_ID" AS "PLAN_ID",
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"."PLAN_NAME" AS "PLAN_NAME",
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"."MER" AS "MER",
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"."PACKAGE" AS "PACKAGE",
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"."LEN" AS "LEN"
+  FROM
+    "oracle"."CUSTOMER_DEMO"."MASTER_PLAN_TABLE_WDATES"
+),
+join_step2 as (
+  SELECT
+    select_step1."LEN" AS "LEN",
+    select_step1."PLAN_NAME" AS "PLAN_NAME",
+    select_step1."PACKAGE" AS "PACKAGE",
+    select_step1."MER" AS "MER",
+    "postgresql"."sample_qa"."fact_subscription_activity"."SBSCRN_ID" AS "SBSCRN_ID",
+    select_step1."PLAN_ID" AS "PLAN_ID"
+  FROM
+    select_step1
+    LEFT OUTER JOIN "postgresql"."sample_qa"."fact_subscription_activity" ON (
+      select_step1."PLAN_ID" = "postgresql"."sample_qa"."fact_subscription_activity"."PLAN_ID"
+    )
+),
+group_by_step3 as (
+  SELECT
+    join_step2."LEN" AS "LEN",
+    join_step2."PACKAGE" AS "PACKAGE",
+    join_step2."PLAN_NAME" AS "PLAN_NAME",
+    sum(MER) as "sum of revenue"
+  FROM
+    join_step2
+  GROUP BY
+    join_step2."LEN",
+    join_step2."PACKAGE",
+    join_step2."PLAN_NAME"
+)
+SELECT
+  *
+FROM
+  group_by_step3
+LIMIT
+  750
